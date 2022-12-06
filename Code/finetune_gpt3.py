619efc899932ef6864f5fce89bfd611caac46b49
jvarshini99 <jvarshini99@gmail.com>
2022-12-05 19:34:49 -0500

    Update finetune_gpt3.py

--- Code/finetune_gpt3.py
+++ Code/finetune_gpt3.py
@@ -2,8 +2,8 @@
 This file is part of the semester project for CS 640 at Boston University.
 It is for experiments to further investigate claims in the paper "Do Androids Laugh at Electric Sheep",
 through several experiments:
-    1) Fine Tuning GPT-3 on a set of New Yorker Caption Contest Captions, to have it describe the setting/joke where those captions would be a punchline
-        Status: Manually creating dataset for the fine-tuning
+    1) Comparing zero-shot learning with few-shot learning on GPT-3 on a set of New Yorker Caption Contest Captions, to have it describe a humourous the setting on the captions and feed it to DALL-E 2 to generate funny pictures.
+        Status: Completed
     2) Running idioms through GPT-3 to see whether it can explain what those idioms mean
         Status: Completed
     3) Feeding the output of the GPT-3 explanations of idioms, into DALL-E 2, to see whether generative models could understand deeper topics in humor (such as idioms)
